ffc62d2babd87eec8c0c2a253aed97bae330c50b
Abhinand Krishna AM <99167984+abhinandkrishna20@users.noreply.github.com>
2022-11-19 19:02:38 +0000

    compiled program

--- First.java
+++ First.java
@@ -1,12 +1,12 @@
 import static java.lang.System.out;
 import java.util.Scanner;
-publi class First {
+public class First {
   public static void main(String[] args){
-  out.println("This is the first program for I/O operations")
+  out.println("This is the first program for I/O operations");
     Scanner input = new Scanner(System.in);
   out.println("Pls Enter your name ");
     String name = input.next();
-    out.println("Welcom Mr/Mrs" + name);
+    out.println("Welcom Mr/Mrs. " + name);
     
   }
 }
